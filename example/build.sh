7e5d1d8c9e54d48d0ffddb0d405a60fa0aec0012
Chad Brewbaker <crb002@Chads-MacBook-Air.local>
2021-05-19 00:59:49 -0500

    add example directory

---
+++ example/build.sh
@@ -0,0 +1,1 @@
+gcc -I../ -O3 random_string.c -o random_string 
---
+++ example/random_string.c
@@ -0,0 +1,36 @@
+#include "chadstr.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+str random_string(size_t length) {
+  chadstr pool = str("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+  size_t randindex;
+  size_t i;
+
+  struct timespec ts;
+  clock_gettime(CLOCK_MONOTONIC, &ts);
+
+  /* using nano-seconds instead of seconds */
+  srand((time_t)ts.tv_nsec);
+
+  char buffer[length];
+
+  for (i = 0; i < length; ++i) {
+    randindex = rand() % pool->len;
+    buffer[i] = pool->data[randindex] ^ ((rand() % 2) ? 0 : 0x20);
+  }
+
+  buffer[i] = '\0';
+
+  return str(buffer);
+}
+
+int main(void) {
+  for (int i = 0; i < 1000; ++i) {
+    chadstr s = random_string(120);
+    puts(str(*s));
+  }
+  return 0;
+}
